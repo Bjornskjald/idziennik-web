34b99a1cfe3a116020a123de486c87261912e1b9
Bjornskjald <github@bjorn.ml>
2017-05-19 18:10:50 +0200

    finally 404 page is working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const 	app = require('express')(),
 		cookieParser = require('cookie-parser'),
 		bodyParser = require('body-parser'),
+		path = require('path'),
 		idziennik = require('idziennik'),
 		Chance = require('chance'),
 		chance = new Chance()
@@ -11,9 +12,9 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'pug')
 app.listen(8080, () => {
-	console.log('App is listening on port 80.')
+	console.log('App is listening on port 8080.')
 })
 
 app.get('*', (req, res) => {
-	res.status(404).sendFile('./res/404.html')
+	res.status(404).sendFile(path.join(__dirname, 'res', '404.html'))
 })
